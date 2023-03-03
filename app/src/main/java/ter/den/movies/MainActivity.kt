bfedb3f537ca3619a91214677bb833e8a1158255
Denis <denter100@mail.ru>
2023-03-03 23:14:18 +0300

    Initial commit

---
+++ app/src/main/java/ter/den/movies/MainActivity.kt
@@ -0,0 +1,21 @@
+package ter.den.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
